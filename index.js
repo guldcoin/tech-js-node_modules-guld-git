692557a165e22c10f6ee1cdfce76b96d6774fdef
Ira Miller <public@iramiller.com>
2018-07-08 11:41:20 -0500

    basic commands

--- index.js
+++ index.js
@@ -2,19 +2,36 @@ const isogit = require('isomorphic-git')
 const spawn = require('guld-spawn')
 const { getJS } = require('guld-env')
 const { getFS } = require('guld-fs')
+const { getName } = require('guld-user')
+const { getAddress } = require('guld-mail')
 const { getPass } = require('guld-pass')
 const home = require('user-home')
 const path = require('path')
+const ISOGITSHORT = {
+  'added': 'A ',
+  '*added': '??',
+  'modified': 'M ',
+  '*modified': ' M',
+  'deleted': 'D ',
+  '*deleted': ' D',
+  'unmodified': null,
+  '*unmodified': null,
+  'ignored': null,
+  'absent': null,
+  '*absent': null
+}
 var fs
+var pfs
 
-async function init () {
+
+/*
   await this.clonep(`ledger/prices`, `https://github.com/guldcoin/ledger-prices.git`).catch(e => console.error)
   await this.clonep(`keys/pgp`, `https://github.com/guldcoin/keys-pgp.git`).catch(e => console.error)
   await this.clonep(`ledger/GULD`, `https://github.com/guldcoin/ledger-guld.git`).catch(e => console.error)
-}
+*/
 
 async function isInitialized () {
-  fs = fs || await getFS()
+  fs = fs || await getFS(false)
   var ps = await Promise.all([
     [`keys/pgp`, `keys-pgp`],
     [`ledger/GULD`, `ledger-GULD`],
@@ -29,10 +46,12 @@ async function isInitialized () {
 }
 
 async function isBehind (p, url) {
-  var exists = await fs.readdir(p).catch(e => false)
+  fs = fs || await getFS(false)
+  pfs = pfs || await getFS()
+  var exists = await pfs.readdir(p).catch(e => false)
   if (exists === false) return exists
   var commit = await log({
-    fs: fs || await getFS(),
+    fs: fs || await getFS(false),
     dir: p,
     gitdir: `${p}/.git`,
     depth: 1
@@ -46,20 +65,146 @@ async function isBehind (p, url) {
   return commit[0].oid !== resp.refs.heads['master']
 }
 
-async function commit (partial, message, time) {
-  var base = partial
-  if (!partial.startsWith(home)) base = path.join(home, partial)
+async function init (cwd) {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
   if ((await getJS()).startsWith('node')) {
-    return spawn('git', undefined, ['-C', path.join(home, partial), 'commit', '-m', message])
+    await spawn('git', undefined, ['-C', cwd, 'init'])    
   } else {
+    await isogit.init({
+      fs: fs || await getFS(false),
+      dir: cwd
+    })
+  }
+}
+
+async function log (cwd, ref='HEAD', depth=1) {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  if ((await getJS()).startsWith('node')) {
+    return (await spawn('git', undefined, ['-C', cwd, 'log', '-n', depth, '--pretty=oneline', ref])).trim().split('\n') 
+  } else {
+    return await isogit.log({
+      fs: fs || await getFS(false),
+      dir: cwd,
+      ref: ref,
+      depth: depth
+    })
+  }
+}
+
+async function status (filepath, cwd='.', short=true) {
+  fs = fs || await getFS(false)
+  if ((await getJS()).startsWith('node')) {
+    var options = ['-C', cwd, 'status']
+    if (short) options.push('-s')
+    if (filepath) options.push(filepath)
+    var result = await spawn('git', undefined, options)
+    if (result !== '') return result.slice(0, -1).split('\n')
+  } else {
+    if (filepath) {
+      var filestat = await isogit.status({
+        fs: fs,
+        dir: cwd,
+        gitdir: path.join(cwd, '.git'),
+        filepath: filepath
+      })
+      if (short) {
+        filestat = statusIsogitToShort(filestat, filepath)
+      }
+      return filestat
+    } else {
+      return listFileStatuses(cwd, short)
+    }
+  }
+}
+
+async function listFileStatuses (cwd='.', short=true) {
+  var fs = fs || await getFS(false)
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  var files = await isogit.listFiles({
+    fs: fs,
+    dir: cwd,
+    gitdir: path.join(cwd, '.git')
+  })
+  var filestats = []
+  await Promise.all(files.map(async (f) => {
+    filestats.push(await isogit.status({
+      fs: fs,
+      dir: cwd,
+      gitdir: path.join(cwd, '.git'),
+      filepath: f
+    }))
+    if (short) filestats[-1] = statusIsogitToShort(filestats[-1], f)
+  }))
+  return filestats
+}
+
+function statusIsogitToShort (stat, filepath) {
+  var short = ISOGITSHORT[stat]
+  if (short) return `${short} ${filepath}`
+}
+
+async function add (filepath, cwd='.') {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  if ((await getJS()).startsWith('node')) {
+    spawn('git', undefined, ['-C', cwd, 'add', filepath || '-A'])
+  } else {
+    if (filepath) {
+      return isogit.add({
+        fs: fs || await getFS(false),
+        dir: cwd,
+        gitdir: path.join(cwd, '.git'),
+        filepath: filepath
+      })
+    } else {
+      var filestats = await listFileStatuses()
+      for (var f in filestats) {
+        if (/^[ ?]{1}[MD?]{1}"]/.match(filestats[f])) {
+           await isogit.add({
+            fs: fs || await getFS(false),
+            dir: cwd,
+            gitdir: path.join(cwd, '.git'),
+            filepath: f
+          })
+        }
+      }
+    }
+  }
+}
+
+async function remove (filepath, cwd='.', cached=false) {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  if ((await getJS()).startsWith('node')) {
+    var options = ['-C', cwd, 'rm', filepath]
+    if (cached) options.push('--cached')
+    else options.push('-f')
+    spawn('git', undefined, options)
+  } else {
+    if (filepath) {
+      return isogit.remove({
+        fs: fs || await getFS(false),
+        dir: cwd,
+        gitdir: path.join(cwd, '.git'),
+        filepath: filepath
+      })
+    } else throw new Error('Filepath required for remove.')
+  }
+}
+
+async function commit (cwd, message='guld', user=null, time=null) {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  user = user || await getName()
+  if ((await getJS()).startsWith('node')) {
+    return spawn('git', undefined, ['-C', cwd, 'commit', '-m', message])
+  } else {
+    time = time || Date.now() / 1000
     return isogit.commit({
-      fs: fs || await getFS(),
-      dir: path.join(home, partial),
-      gitdir: path.join(home, partial, '.git'),
-      message: `guld app transaction`,
+      fs: fs || await getFS(false),
+      dir: cwd,
+      gitdir: path.join(cwd, '.git'),
+      message: message,
       author: {
-        name: observer['fullname'],
-        email: observer['mail'],
+        name: user,
+        email: await getAddress(user),
         date: new Date(time * 1000),
         timestamp: time
       }
@@ -67,17 +212,16 @@ async function commit (partial, message, time) {
   }
 }
 
-async function pull (partial, remote = 'origin', ref = 'master') {
-  var base = partial
-  if (!partial.startsWith(home)) base = path.join(home, partial)
+async function fetch (cwd, remote, ref) {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
   if ((await getJS()).startsWith('node')) {
-    return spawn('git', undefined, ['-C', path.join(home, partial), 'pull', remote, ref])
+    return spawn('git', undefined, ['-C', cwd, 'fetch', remote, ref])
   } else {
     var creds = await getPass(`${user}/git/github`)
-    return isogit.pull({
-      fs: fs || await getFS(),
-      dir: path.join(home, partial),
-      gitdir: path.join(home, partial, '.git'),
+    return isogit.fetch({
+      fs: fs || await getFS(false),
+      dir: cwd,
+      gitdir: path.join(cwd, '.git'),
       ref: ref,
       authUsername: creds.login,
       authPassword: creds.password
@@ -85,18 +229,38 @@ async function pull (partial, remote = 'origin', ref = 'master') {
   }
 }
 
-async function push (partial, remote = 'origin', ref = 'master') {
-  var base = partial
-  if (!partial.startsWith(home)) base = path.join(home, partial)
-  fs = fs || await getFS()
+async function pull (cwd, remote, ref) {
+  remote = remote || await getName()
+  ref = ref || await getName()
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  if ((await getJS()).startsWith('node')) {
+    return spawn('git', undefined, ['-C', cwd, 'pull', remote, ref])
+  } else {
+    var creds = await getPass(`${user}/git/github`)
+    return isogit.pull({
+      fs: fs || await getFS(false),
+      dir: cwd,
+      gitdir: path.join(cwd, '.git'),
+      ref: ref,
+      authUsername: creds.login,
+      authPassword: creds.password
+    })
+  }
+}
+
+async function push (cwd, remote, ref) {
+  remote = remote || await getName()
+  ref = ref || await getName()
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
+  fs = fs || await getFS(false)
   if ((await getJS()).startsWith('node')) {
     spawn('git', undefined, ['-C', path.join(home, partial), 'push', remote, ref])
   } else {
     var creds = await getPass(`${user}/git/github`)
     return isogit.push({
-      fs: fs || await getFS(),
-      dir: path.join(home, partial),
-      gitdir: path.join(home, partial, '.git'),
+      fs: fs || await getFS(false),
+      dir: cwd,
+      gitdir: path.join(cwd, '.git'),
       remote: remote,
       ref: ref,
       authUsername: creds.login,
@@ -105,32 +269,16 @@ async function push (partial, remote = 'origin', ref = 'master') {
   }
 }
 
-async function add (partial, filepath) {
-  var base = partial
-  if (!partial.startsWith(home)) base = path.join(home, partial)
+async function clone (cwd, filepath, url) {
+  if (!cwd.startsWith(home)) cwd = path.resolve(cwd)
   if ((await getJS()).startsWith('node')) {
-    spawn('git', undefined, ['-C', path.join(home, partial), 'add', filepath || '-A'])
-  } else {
-    return isogit.add({
-      fs: fs || await getFS(),
-      dir: base,
-      gitdir: path.join(base, '.git'),
-      filepath: filepath
-    })
-  }
-}
-
-async function clone (partial, url) {
-  var base = partial
-  if (!partial.startsWith(home)) base = path.join(home, partial)
-  if ((await getJS()).startsWith('node')) {
-    spawn('git', undefined, ['-C', base, 'add', filepath])
+    spawn('git', undefined, ['-C', cwd, 'clone', filepath])
   } else {
     var creds = await getPass(`${user}/git/github`)
     return isogit.clone({
-      fs: fs || await getFS(),
-      dir: base,
-      gitdir: path.join(base, '.git'),
+      fs: fs || await getFS(false),
+      dir: cwd,
+      gitdir: path.join(cwd, '.git'),
       url: url,
       singleBranch: true,
       depth: 1,
@@ -140,15 +288,16 @@ async function clone (partial, url) {
   }
 }
 
-async function clonep (partial, url, ref = 'master') {
+async function clonep (partial, url, ref) {
   /*
    * Clone or pull if already exists.
    */
+  pfs = pfs || await getFS()
   var p = partial
   if (!partial.startsWith(home)) p = path.join(home, partial)
   var stats
   try {
-    stats = await fs.stat(p)
+    stats = await pfs.stat(p)
   } catch (e) {
     return clone(partial, url)
   }
@@ -166,10 +315,15 @@ async function clonep (partial, url, ref = 'master') {
 }
 
 module.exports = {
-  // push: push,
-  // clone: clone,
-  // pull: pull,
-  // commit: commit,
-  add: add
+  init: init,
+  status: status,
+  log: log,
+  add: add,
+  remove: remove,
+  commit: commit,
+  clone: clone,
+  fetch: fetch,
+  push: push,
+  pull: pull
 }
 
--- test.js
+++ test.js
@@ -1,13 +1,66 @@
-/* global describe:false it:false before:false */
+/* eslint-env node, mocha */
 const assert = require('chai').assert
-const btc = require('./index.js')
+const { init, status, add, remove, commit, log } = require('./index.js')
+const { getFS } = require('guld-fs')
+var fs
 
-describe('btc', function () {
-  it('generate', async function () {
-    this.randkey = await btc.generate()
-    assert.exists(this.randkey)
-    assert.exists(this.randkey.publicAddress)
-    assert.exists(this.randkey.privateWif)
-    assert.notEqual((await btc.listKeys()).indexOf(this.randkey.publicAddress), -1)
+describe('guld-git', function () {
+  before(async () => {
+    fs = fs || await getFS()
+    await fs.mkdirp('testrepo')
+  })
+  after(async () => {
+    await fs.rimraf('testrepo')
+  })
+  it('init', async function () {
+    var stat = await init('testrepo')
+    assert.isTrue((await fs.stat('testrepo/.git')).isDirectory())
+  })
+  it('status empty', async function () {
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat, undefined)
+  })
+  it('untracked new file status', async function () {
+    await fs.writeFile('testrepo/file', '')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat[0], '?? file')
+  })
+  it('add', async function () {
+    await add('file', 'testrepo')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat[0], 'A  file')
+  })
+  it('remove', async function () {
+    await remove('file', 'testrepo')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat, undefined)
+  })
+  it('add all', async function () {
+    await fs.writeFile('testrepo/file', '')
+    await fs.writeFile('testrepo/file1', '')
+    await add(undefined, 'testrepo')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat[0], 'A  file')
+    assert.equal(stat[1], 'A  file1')
+  })
+  it('commit', async function () {
+    await commit('testrepo', message='test message')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat, undefined)
+  }).timeout(5000)
+  it('log', async function () {
+    var l = await log('testrepo')
+    assert.equal(l.length, 1)
+    assert.isTrue(l[0].endsWith(' test message'))
+  })
+  it('modify', async function () {
+    await fs.writeFile('testrepo/file', 'a')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat[0], ' M file')
+  })
+  it('add modified', async function () {
+    await add('file', 'testrepo')
+    var stat = await status(undefined, 'testrepo')
+    assert.equal(stat[0], 'M  file')
   })
 })
